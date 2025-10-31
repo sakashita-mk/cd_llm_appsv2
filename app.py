c8c1b59c2c7612a0b85332cc0e1656a6df67c9e9
Mk <sakashita.masanori@gmail.com>
2025-10-31 15:19:18 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.set_page_config(page_title="宇宙事業デザインセッションプラットフォーム（仮）, layout="wide")
+st.set_page_config(page_title="宇宙事業デザインセッションプラットフォーム（仮）", layout="wide")
 st.title("宇宙事業デザインセッションプラットフォーム（仮）")
 t1, t2, t3 = st.tabs(["Tab1: ユースケース入力", "Tab2: 構成方針提示", "Tab3: 統合プラン（Under Construction）"])
 with t1:
